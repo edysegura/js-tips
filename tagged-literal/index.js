5ec3a59107a0a9f8431deeb852e58bdb3faa947a
Edy Segura <ednardo@inatel.br>
2018-02-12 11:54:23 -0200

    Added tagged literal example

---
+++ tagged-literal/index.js
@@ -0,0 +1,11 @@
+const myTag = (template, ...values) => {
+  return template.reduce((previousTemplate, nextTemplate, index) => {
+    return `${previousTemplate}${values[index - 1].toUpperCase()}${nextTemplate}`
+  })
+}
+
+ const name = "Edy Segura"
+ const city = "Santa Rita do Sapucaí"
+
+ const output = myTag`Hi! I'm ${name} and I live in ${city}.`
+ console.log(output)
