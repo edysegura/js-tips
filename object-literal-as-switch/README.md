30341be3ea64327cbd343a381a0300a18eba2f60
Edy Segura <ednardo@inatel.br>
2018-02-18 18:47:50 -0300

    Added Object Literal as switch case example

---
+++ object-literal-as-switch/README.md
@@ -0,0 +1,7 @@
+# Replacing switch statements with Object literals
+
+This example demonstrates how you can replace JavaScript switch case by Object literals.
+
+## References
+
+- [Replacing switch statements with Object literals](https://toddmotto.com/deprecating-the-switch-statement-for-object-literals/)
---
+++ object-literal-as-switch/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+function getErrorMessageSwitch(errorCode) {
+  let errorMessage = ''
+  switch(errorCode) {
+    case '001':
+      errorMessage = 'Error message from code 001'
+    break
+    case '002':
+      errorMessage = 'Error message from code 002'
+    break
+    case '003':
+      errorMessage = 'Error message from code 003'
+    break
+    case '004':
+      errorMessage = 'Error message from code 004'
+    break
+    default:
+      errorMessage = 'No error message was found'
+  }
+  return errorMessage
+}
+
+function getErrorMessageLiteral(errorCode) {
+  const errorMessages = {
+    '001': 'Error message from code 001',
+    '002': 'Error message from code 002',
+    '003': 'Error message from code 003',
+    '004': 'Error message from code 004',
+    'default': 'No error message was found',
+  }
+  return errorMessages[errorCode] || errorMessages['default']
+}
+
+console.log(getErrorMessageLiteral('004'))
