c372839d3edd894d7d81c7b906c265735f16d66c
Carlos Augusto Grispan <cagrispan@gmail.com>
2018-04-04 15:42:23 -0300

    Add text default value

--- destructuring-with-default-arguments/index.js
+++ destructuring-with-default-arguments/index.js
@@ -1,11 +1,11 @@
 'use strict'
 
-function doSomething({ text = '', line = 0, truncate = 100} = {}) {
+function doSomething({ text = 'Default Text', line = 0, truncate = 100} = {}) {
   console.log(text, line, truncate)
 }
+
+doSomething()
 
 doSomething({
   text: 'Edy Segura'
 })
-
-doSomething()
