7ff05d2c21e0c8a03c2cbba5a57c57ec912070ec
Edy Segura <edysegura@gmail.com>
2018-08-13 21:59:19 -0300

    Creating enum with proxy

---
+++ enum-with-proxy/index.js
@@ -0,0 +1,16 @@
+const MyEnum = new Proxy(
+  {},
+  {
+    get(target, property, receiver) {
+      return property
+    }
+  }
+)
+
+console.log(MyEnum.PropertyOne)
+console.log(MyEnum.PropertyTwo)
+console.log(MyEnum.PropertyThree)
+
+MyEnum.PropertyOne = 'Whatever'
+console.log(MyEnum.PropertyOne)
+console.log(MyEnum.PropertyOne === MyEnum.PropertyOne)
