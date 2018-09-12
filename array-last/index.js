a5bd603b3eb0909e0067a516f65e15cef9ad020a
Edy Segura <edysegura@gmail.com>
2018-09-12 20:25:26 -0300

    Refactoring

--- array-last/index.js
+++ array-last/index.js
@@ -1,19 +1,19 @@
 const names = [
-  "Cloud Strife",
-  "Tifa Lockhart",
-  "Aerith Gainsborough",
-  "Barret Wallace",
-  "Yuffie Kisaragi",
-  "Vincent Valentine",
-  "Zack Fair",
-  "Red XIII",
-  "Cait Sith",
-  "Cid Highwind"
+  'Cloud Strife',
+  'Tifa Lockhart',
+  'Aerith Gainsborough',
+  'Barret Wallace',
+  'Yuffie Kisaragi',
+  'Vincent Valentine',
+  'Zack Fair',
+  'Red XIII',
+  'Cait Sith',
+  'Cid Highwind'
 ]
 
 const [ firstOne ] = names
+const lastOne = names[ names.length - 1 ]
 
-const lastOne = names[names.length - 1]
 // const [ lastOne ] = [...names].reverse()
 // const lastOne = [...names].pop()
 
