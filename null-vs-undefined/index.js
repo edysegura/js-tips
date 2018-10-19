4914922c25660488a1a34ef374968b50c866c5ae
Edy Segura <edysegura@gmail.com>
2018-10-19 15:42:20 -0300

    Added null vs undefined tip

---
+++ null-vs-undefined/index.js
@@ -0,0 +1,8 @@
+const dataStructure = {
+  a: null,
+  b: undefined
+}
+
+console.log('+null: ', +null)
+console.log('+undefined: ', +undefined)
+console.log(JSON.stringify(dataStructure), 'Where are the property b?')
