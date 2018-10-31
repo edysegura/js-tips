e7768cf7cf3437c761279f2d2b1763f0c8f5119a
Edy Segura <edysegura@gmail.com>
2018-10-31 10:49:54 -0300

    Added setter and getter tip

---
+++ setter-getter/index.js
@@ -0,0 +1,20 @@
+const language = {
+  currentLanguage: '',
+  log: [],
+
+  set current(name) {
+    this.currentLanguage = name
+    this.log.push(name)
+  },
+
+  get current() {
+    return this.currentLanguage
+  }
+}
+
+language.current = 'PT-BR'
+language.current = 'JP'
+language.current = 'EN-US'
+
+console.log('Current Language: ', language.current)
+console.log(language.log)
