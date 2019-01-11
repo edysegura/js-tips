c0f7b0118dd8bef1ccaadadc2a74585d3408430f
Edy Segura <edysegura@gmail.com>
2019-01-10 23:21:30 -0200

    Refactoring unique array example

--- array-unique/index.js
+++ array-unique/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const getUniqueValues = list => [ ...new Set(list) ]
+const getUniqueValues = list => Array.from(new Set(list))
 const uniqueValues = getUniqueValues([ 'Lidy', 'Davi', 'Edy', 'Davi', 'Davi' ])
 
 console.log(uniqueValues)
