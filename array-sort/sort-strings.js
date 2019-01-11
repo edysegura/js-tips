13334b73757d33d1b849031a4277e0f3b843a8a3
Edy Segura <edysegura@gmail.com>
2019-01-11 17:03:18 -0200

    Added example reference

--- array-sort/sort-strings.js
+++ array-sort/sort-strings.js
@@ -5,3 +5,5 @@ const byAlphabeticalOrder = (wordA, wordB) => ('' + wordA).localeCompare(wordB)
 
 wordList.sort(byAlphabeticalOrder)
 console.log(wordList) // [ 'Abacate', 'Maçã', 'Pitaya', 'Uva' ]
+
+// Ref.: https://stackoverflow.com/questions/51165/how-to-sort-strings-in-javascript
