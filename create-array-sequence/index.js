a1a1253ccc11ea1bae7d6436dac87c74840b95df
Edy Segura <edysegura@gmail.com>
2019-02-13 16:45:44 -0200

    Update code style

--- create-array-sequence/index.js
+++ create-array-sequence/index.js
@@ -2,7 +2,7 @@
 
 const n = 10
 const generatedListOne = Array.from(Array(n).keys())
-const generatedListTwo = [...Array(n).keys()]
+const generatedListTwo = [ ...Array(n).keys() ]
 
-console.log('generatedListOne: ', generatedListOne);
-console.log('generatedListTwo: ', generatedListTwo);
+console.log('generatedListOne: ', generatedListOne)
+console.log('generatedListTwo: ', generatedListTwo)
