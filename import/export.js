06fb124657b5a3d5d86ebcaa1f620a61f2a83448
Edy Segura <edysegura@gmail.com>
2019-10-04 14:00:00 +0100

    Add export example with import

---
+++ import/export.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const js = `export default 'Returned value'`
+const dataUri = 'data:text/javascript;charset=utf-8,' + encodeURIComponent(js)
+
+import(dataUri)
+  .then((namespaceObject) => {
+    console.log(namespaceObject.default, 'Returned value')
+  });
