2f785b0b25f99a8702233eaf34df3fc76a9ae80c
Edy Segura <edysegura@gmail.com>
2019-11-11 22:28:48 -0300

    Implement getAbsoluteUrl example

--- get-absolute-url/index.js
+++ get-absolute-url/index.js
@@ -1,12 +1,19 @@
 const getAbsoluteUrl = (() => {
   let link;
 
-  return (relativePath) => {
-    if(!link) link = document.createElement('a')
+  return (relativePath = '') => {
+    if (!link) link = document.createElement('a')
     link.href = relativePath
     return link.href
   }
 })()
 
+const input = document.querySelector('input')
+const button = document.querySelector('button')
 const output = document.querySelector('output')
-output.textContent = getAbsoluteUrl('/relative-path')
+
+const buildAbsoluteUrl = () => {
+  output.textContent = getAbsoluteUrl(input.value)
+}
+
+button.addEventListener('click', buildAbsoluteUrl)
