f920fb824cb18c25c4f6161d10bca25d81700ead
Edy Segura <edysegura@gmail.com>
2020-03-06 17:10:12 -0300

    Refactor index.js

--- random-alphanumeric/index.js
+++ random-alphanumeric/index.js
@@ -1,11 +1,11 @@
 'use strict'
 
 function alphaRandom(stringLength) {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
+  let text = "";
   for (var i = 0; i < stringLength; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
-  return text.split(/(\w{4})/).join('-');
+  return text;
 }
 
 console.log(alphaRandom(32))
