250e9bdb003c29199c88989eea312329d698d690
Edy Segura <edysegura@gmail.com>
2020-03-18 16:41:02 -0300

    Refactor index.js
    The refactor was made in order to have a visual feedback on index.html

--- browser-idle-time/index.js
+++ browser-idle-time/index.js
@@ -1,4 +1,10 @@
 import { IdleTimer } from './idle-timer.js'
 
+function showExpiredMessage(data) {
+  const p = document.getElementById('expired-message')
+  p.textContent = 'Session expired!!!! ' + data
+  p.classList.add('expired')
+}
+
 new IdleTimer()
-  .then((data) => console.log(data))
+  .then(showExpiredMessage)
