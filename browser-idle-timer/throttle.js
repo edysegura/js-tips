fd5df81ce2b0765013ec960e06bf6dc1cbaca129
Edy Segura <edysegura@gmail.com>
2020-03-25 22:54:45 -0300

    Add throttle.js
    Remove debounce.js and added the throttle.js

---
+++ browser-idle-timer/throttle.js
@@ -0,0 +1,15 @@
+/**
+ * Exectue an action which needs to be throttled
+ * @param {*} action it is the function that need to be throttled
+ * @param {*} delay the time interval in milliseconds
+ */
+export function throttle(action, delay) {
+  let timerId
+  return (...params) => {
+    if (timerId) return
+    timerId = setTimeout(() => {
+      action(...params)
+      timerId = null
+    }, delay)
+  }
+}
