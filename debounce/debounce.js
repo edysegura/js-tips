3db08a42045a092ef9562db3b6c4ec06362d3c63
Edy Segura <edysegura@gmail.com>
2020-04-01 17:29:22 -0300

    Update debounce documentation

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -1,12 +1,13 @@
 /**
- * It limits the rate at which a function can fire
- * @param {Function} action
- * @param {Number} wait
+ * It limits the rate at which a function can be fired
+ * @param {Function} action An action to be performed
+ * @param {Number} wait Time in milliseconds to wait before perform the action
  */
 export function debounce(action, wait) {
   let timer = null
   return function () {
     clearTimeout(timer)
+    // TODO we need to consider the arguments
     timer = setTimeout(action, wait)
   }
 }
