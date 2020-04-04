c70ccb56bcd3de042debfe1a76685b2bf24ab20d
Edy Segura <edysegura@gmail.com>
2020-04-03 22:01:39 -0300

    Add throttle.js implementation

---
+++ throttle/throttle.js
@@ -0,0 +1,15 @@
+/**
+ * Exectue an action which needs to be throttled
+ * @param { Function } action An action to be performed that need to be throttled
+ * @param { number } wait Time in milliseconds to wait before perform the action
+ */
+export function throttle(action, wait) {
+  let timerId
+  return (...params) => {
+    if (timerId) return
+    timerId = setTimeout(() => {
+      action(...params)
+      timerId = null
+    }, wait)
+  }
+}
