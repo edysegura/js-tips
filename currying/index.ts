ad68435416c535adb80ea9501b5853dfbc6ded29
Edy Segura <edysegura@gmail.com>
2020-11-19 21:39:38 -0300

    Add TypeScript example

---
+++ currying/index.ts
@@ -0,0 +1,15 @@
+
+function validation(mode: 'Max' | 'Min', threshold: number): Function {
+  if (mode === 'Max') {
+    return (input: number) => input > threshold
+  } else if (mode === 'Min') {
+    return (input: number) => input < threshold
+  }
+  return () => threshold
+}
+
+const max = validation('Max', 21)
+const min = validation('Min', 18)
+
+console.log('12 > 21 =', max(12))
+console.log('22 > 21 =', max(22))
