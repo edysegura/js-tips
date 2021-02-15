14286fa10654210e78097fed1044ab1a70d6b1f3
Edy Segura <edysegura@gmail.com>
2021-02-15 06:34:43 -0300

    Update counter script

--- counter/index.js
+++ counter/index.js
@@ -5,9 +5,17 @@ function updateUI(counter) {
   output.textContent = counter
 }
 
-updateUI(0)
+/**
+ * How to make the counter work accurately and efficiently across browsers
+ *
+ * Most of counters loose accuracy over time! That is why of the implementation below
+ * check more details on this link: https://www.youtube.com/watch?v=MCi6AZMkxcU
+ */
+const start = Date.now()
+const delay = 1000
 
-let counter = 0
 setInterval(() => {
-  updateUI(counter++)
-}, 1000)
+  const elapsed = Date.now() - start
+  const seconds = Math.floor(elapsed / 1000)
+  updateUI(seconds)
+}, delay)
