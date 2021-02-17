4c07a99eacc473b9d6be3a102291881d42edc479
Edy Segura <edysegura@gmail.com>
2021-02-16 21:34:48 -0300

    Add reference to object literal as switch example

--- object-literal-as-switch/index.js
+++ object-literal-as-switch/index.js
@@ -21,15 +21,20 @@ function getErrorMessageSwitch(errorCode) {
   return errorMessage
 }
 
+/**
+ * Instead of using switch case we can use object literal
+ * Ref.: https://ultimatecourses.com/blog/deprecating-the-switch-statement-for-object-literals
+ */
 function getErrorMessageLiteral(errorCode) {
   const errorMessages = {
     '001': 'Error message from code 001',
     '002': 'Error message from code 002',
     '003': 'Error message from code 003',
     '004': 'Error message from code 004',
-    default: 'No error message was found'
+    'default': 'No error message was found'
   }
   return errorMessages[ errorCode ] || errorMessages.default
 }
 
-console.log(getErrorMessageLiteral('004'))
+console.log(getErrorMessageLiteral('003'))
+console.log(getErrorMessageLiteral('x'))
