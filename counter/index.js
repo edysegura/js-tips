055eeefc960d8d9d62e49b790fbbd1ccde48bb17
Edy Segura <edysegura@gmail.com>
2021-02-19 23:18:39 -0300

    Update comments on counter example

--- counter/index.js
+++ counter/index.js
@@ -11,9 +11,9 @@ function updateUI(second) {
  * Most of counters loose accuracy over time! That is why of the implementation below
  * check more details on this link: https://www.youtube.com/watch?v=MCi6AZMkxcU
  *
- * v1 regular counter
- * v2 using object Date to be in sync
- * v3 using requestAnimationFrame
+ * v1 regular counter ( Out of sync on long running )
+ * v2 using object Date to be in sync ( Out of sync on long running )
+ * v3 using requestAnimationFrame ( CPU consuming )
  */
 const start = Date.now()
 function frame() {
