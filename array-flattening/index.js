3451b85ef4f3490010295cfed5f31727af19ac37
Edy Segura <ednardo.segura@mundiale.com.br>
2021-07-30 22:25:54 -0300

    refactor: add Array.flat() example

--- array-flattening/index.js
+++ array-flattening/index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const listOfList = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 const flattening = (accumulator, currentValue) => [
@@ -9,7 +7,9 @@ const flattening = (accumulator, currentValue) => [
 
 const flatOne = listOfList.reduce(flattening)
 const flatTwo = [].concat.apply([], listOfList)
+const flatThree = listOfList.flat()
 
 console.log(listOfList)
 console.log(flatOne)
 console.log(flatTwo)
+console.log(flatThree)
