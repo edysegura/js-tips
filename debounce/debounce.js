85668e66317ec7a6c13e3306cb7d19350b9080e6
Edy Segura <edysegura@gmail.com>
2021-08-02 21:06:11 -0300

    refactor: rename action param to performAction

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -1,14 +1,14 @@
 /**
  * It limits the rate at which a function can be fired
- * @param {Function} action An action to be performed
+ * @param {Function} performAction An action to be performed
  * @param {Number} wait Time in milliseconds to wait before perform the action
  */
-export function debounce(action, wait) {
+export function debounce(performAction, wait) {
   let timer = null
   return function (...params) {
     clearTimeout(timer)
     timer = setTimeout(
-      () => action(...params),
+      () => performAction(...params),
       wait
     )
   }
