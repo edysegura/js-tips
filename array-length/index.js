b1e3e7983a8a51b4435ce8764a502bcf60bc7996
Edy Segura <edysegura@gmail.com>
2021-08-02 21:14:02 -0300

    chore: fixing a comment type

--- array-length/index.js
+++ array-length/index.js
@@ -3,11 +3,11 @@
 const myList = [ 1, 2, 3, 4 ]
 console.log(myList)
 
-//removing the last two positions
+// removing the last two positions
 const dropLastTwo = 2
 myList.length = dropLastTwo
 console.log(myList)
 
-//cleanning the list
+// cleaning the list
 myList.length = 0
 console.log(myList)
