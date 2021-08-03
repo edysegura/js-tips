cc65060457ce538ef00dab3fe4823b6dca8c7ea6
Edy Segura <edysegura@gmail.com>
2021-08-02 21:25:36 -0300

    refactor: rename action param to increase semantic

--- browser-idle-timer/throttle.js
+++ browser-idle-timer/throttle.js
@@ -1,14 +1,14 @@
 /**
- * Exectue an action which needs to be throttled
- * @param { Function } action it is the function that need to be throttled
+ * Execute an action which needs to be throttled
+ * @param { Function } performAction it is the function that need to be throttled
  * @param { number } wait the wait in milliseconds
  */
-export function throttle(action, wait) {
+export function throttle(performAction, wait) {
   let timerId
   return (...params) => {
     if (timerId) return
     timerId = setTimeout(() => {
-      action(...params)
+      performAction(...params)
       timerId = null
     }, wait)
   }
