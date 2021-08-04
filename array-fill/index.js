6db2b613aaab20716b0b6124fb3b837926950f79
Edy Segura <ednardo.segura@mundiale.com.br>
2021-08-03 22:20:22 -0300

    chore: encapsulate Array.fill to a semantic function

--- array-fill/index.js
+++ array-fill/index.js
@@ -1,2 +1,7 @@
-const numbers = Object.keys(new Array(10).fill(null)).map(Number)
-console.log(numbers)
+function generateNumbers(size = 0) {
+    // it is just a possibility, don't use this on production as it isn't performatic
+    const numbers = Object.keys(new Array(size).fill(null)).map(Number)
+    return numbers
+}
+
+console.log(generateNumbers(10))
