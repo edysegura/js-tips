ca7842a473ee0feaa1f6eab02d1342ac1ec4deec
Edy Segura <ednardo.segura@mundiale.com.br>
2021-08-09 07:11:45 -0300

    chore: add references to immutability example

--- immutability/index.js
+++ immutability/index.js
@@ -6,7 +6,12 @@ const snorlax = {
   type: 'Normal'
 }
 
+// Object.freeze() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+// This method just do a shallow freeze, in order to do a deepFreeze there is an example on link above
 Object.freeze(snorlax)
+// there are two others similar methos
+// Object.seal() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/seal
+// Object.preventExtension()- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/preventExtensions
 
 snorlax.name = 'Jigglypuff'
 console.log(snorlax.name)
