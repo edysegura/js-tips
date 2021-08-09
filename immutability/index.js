b41f5caf3918f5b06eae75292883df78c954b0aa
Edy Segura <ednardo.segura@mundiale.com.br>
2021-08-09 07:32:18 -0300

    refactor: add new property to snorlax

--- immutability/index.js
+++ immutability/index.js
@@ -3,15 +3,20 @@
 const snorlax = {
   number: 143,
   name: 'Snorlax',
-  type: 'Normal'
+  type: 'Normal',
+  evolution: {
+    munchlax: '446'
+  }
 }
 
 // Object.freeze() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
 // This method just do a shallow freeze, in order to do a deepFreeze there is an example on link above
 Object.freeze(snorlax)
+Object.freeze(snorlax.evolution)
 // there are two others similar methos
 // Object.seal() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/seal
 // Object.preventExtension()- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/preventExtensions
 
 snorlax.name = 'Jigglypuff'
-console.log(snorlax.name)
+snorlax.evolution.munchlax = '001'
+console.log(snorlax)
