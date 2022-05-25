d5f2f76d02895c289628cc26f3255ac783fe3331
Edy Segura <edysegura@gmail.com>
2022-05-24 22:56:42 -0300

    feat: add references

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,3 +1,4 @@
+// find other ways here https://www.samanthaming.com/pictorials/how-to-capitalize-a-string/
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
