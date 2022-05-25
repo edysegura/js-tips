eb957063e41a89085fc89ffc7f15308c8422965d
Edy Segura <edysegura@gmail.com>
2022-05-24 22:58:22 -0300

    feat: add css alternative

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,4 +1,5 @@
 // find other ways here https://www.samanthaming.com/pictorials/how-to-capitalize-a-string/
+// you can also use CSS for that text-transform: capitalize
 function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
