6772a523749f88a091314ba0725cbb55642542d5
Edy Segura <edysegura@gmail.com>
2022-05-24 23:04:23 -0300

    feat: functional programming flavor in capitalize example

--- capitalize/index.js
+++ capitalize/index.js
@@ -1,7 +1,17 @@
 // find other ways here https://www.samanthaming.com/pictorials/how-to-capitalize-a-string/
 // you can also use CSS for that text-transform: capitalize
-function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+function titleCase(string) {
+  return string
+    .toLowerCase()
+    .split(' ')
+    .map(capitalize)
+    .join(' ')
 }
 
 console.log(capitalize('edy segura'))
+console.log(titleCase('edy segura'))
+console.log(titleCase('edY segUra'))
