f3ec2fce4ced4881c49ff58895293a756ff9717b
Edy Segura <edysegura@gmail.com>
2022-05-25 22:31:44 -0300

    feat: add slugify example

---
+++ slugify/index.js
@@ -0,0 +1,9 @@
+const slugify = (string) =>
+  string
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+
+console.log(slugify('Hello World!')) // 'hello-world'
