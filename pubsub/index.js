4ebea0edbdd76ae8636a919eaa7ae65dc51b2219
Edy Segura <edysegura@gmail.com>
2023-08-24 22:28:45 -0300

    feat: update pub/sub example

--- pubsub/index.js
+++ pubsub/index.js
@@ -8,8 +8,15 @@ const pubSub = {
     if (this.events[event])
       this.events[event].forEach((callback) => callback(data))
   },
+  unsubscribe(event, callback) {
+    if (this.events[event])
+      this.events[event] = this.events[event].filter((cb) => cb !== callback)
+  },
 }
 
+const namedCallback = (data) => console.log(`[Named callback] ${data}`)
 pubSub.subscribe('update', (data) => console.log(data))
+pubSub.subscribe('update', namedCallback)
 pubSub.publish('update', 'Some update')
+pubSub.unsubscribe('update', namedCallback)
 pubSub.publish('update', 'Another update')
