4fdb3d86bdedb2c2000de92271152900638c63e2
Edy Segura <edysegura@gmail.com>
2026-04-09 22:05:26 -0300

    feat: add more options to function properties

--- function-properties/index.js
+++ function-properties/index.js
@@ -11,3 +11,30 @@ console.log(generator())
 console.log(generator())
 console.log(generator())
 console.log(generator())
+console.log(Object.keys(generator))
+
+// -----------------------------------------
+
+const counterSymbol = Symbol('counter')
+
+function generator2() {
+  if (!generator2[counterSymbol]) {
+    generator2[counterSymbol] = 0
+  }
+  return ++generator2[counterSymbol]
+}
+
+console.log(generator2())
+console.log(generator2())
+console.log(Object.keys(generator2))
+
+// -----------------------------------------
+
+const generator3 = (() => {
+  let counter = 0
+  return () => ++counter
+})()
+
+console.log(generator3()) // 1
+console.log(generator3()) // 2
+// counter is completely inaccessible
