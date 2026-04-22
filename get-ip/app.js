7e31712d56e54cb65e78f4e40b187ecf303a69b3
Edy Segura <edysegura@gmail.com>
2026-04-22 17:03:13 -0300

    chore: remove unnecessary line

--- get-ip/app.js
+++ get-ip/app.js
@@ -4,7 +4,6 @@ async function main() {
   const p = document.querySelector('p')
   const response = await fetch('https://httpbin.org/ip')
   const ip = await response.json()
-
   p.textContent = ip.origin
 }
 
