450f98ab53014ab86f12c0a1aba64c8d19341071
Edy Segura <edysegura@gmail.com>
2026-04-29 17:11:58 -0300

    refactor: using the window context

--- jwt-decode/app.js
+++ jwt-decode/app.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('input')
 const pre = document.querySelector('pre')
 
-const parsedJWT = JSON.parse(atob(input.value.split('.')[1]))
+const parsedJWT = JSON.parse(window.atob(input.value.split('.')[1]))
 const json = JSON.stringify(parsedJWT, null, 2)
 pre.textContent = json
