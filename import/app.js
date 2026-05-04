e312eab747346ebd3993b2b8e5561051e9e4c8b7
Edy Segura <edysegura@gmail.com>
2026-05-04 17:03:58 -0300

    refactor: update the messages from import example

--- import/app.js
+++ import/app.js
@@ -1,8 +1,8 @@
-const evaluationJsMethod1 = `console.log('Hello Everyone')`
+const evaluationJsMethod1 = `console.log('Hello Everyone from an isolated module')`
 const dataUriFromMethod1 = `data:text/javascript;charset=utf-8,${encodeURIComponent(evaluationJsMethod1)}`
 import(dataUriFromMethod1)
 
-const evaluationJsMethod2 = `export default 'Returned value'`
+const evaluationJsMethod2 = `export default 'Returned value from an isolated module'`
 const dataUriFromMethod2 = `data:text/javascript;charset=utf-8,${encodeURIComponent(evaluationJsMethod2)}`
 import(dataUriFromMethod2).then((module) => {
   console.log(module.default) // Returned value
