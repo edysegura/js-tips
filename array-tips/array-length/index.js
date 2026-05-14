253235bff2a513358d04311b6b026b6a98423b8c
Edy Segura <edysegura@gmail.com>
2026-05-14 17:21:28 -0300

    refactor: move array tips to array folder

---
+++ array-tips/array-length/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const myList = [1, 2, 3, 4]
+console.log(myList)
+
+// removing the last one
+const dropLastOne = 3
+myList.length = dropLastOne
+console.log(myList)
+
+// cleaning the list
+myList.length = 0
+console.log(myList)
